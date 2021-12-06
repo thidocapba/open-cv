e8db92f2283532cf7f266b727d8ca1579e27f213
Dung Lo <lodung842001@gmail.com>
2021-12-06 22:22:16 +0700

    22:22

--- basic.py
+++ basic.py
@@ -20,4 +20,16 @@ cv.imshow('Canny Edge', canny)
 dilated = cv.dilate(canny, (7, 7), iterations = 3)
 cv.imshow('Dilated', dilated)
 
+# Eroding
+eroded = cv.erode(dilated, (7, 7), iterations = 1)
+cv.imshow("Eroded", eroded)
+
+# Resize
+resized = cv.resize(img, (500, 500), interpolation = cv.INTER_CUBIC)
+cv.imshow("Resized", resized)
+
+# Cropping
+cropped = img[50:200, 200:400]
+cv.imshow("Cropped", cropped)
+
 cv.waitKey(0)
